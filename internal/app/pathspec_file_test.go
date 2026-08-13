d066af515a7f315175877d9c3adb7733181f45bf
Damon Blais <damon.blais@gmail.com>
2026-08-12 19:46:37 -0700

    test(app): pin pathspec-file-nul wiring

--- internal/app/pathspec_file_test.go
+++ internal/app/pathspec_file_test.go
@@ -53,3 +53,38 @@ func TestRunPathspecFromFileMatchesPositionals(t *testing.T) {
 	qt.Assert(t, qt.StringContains(head, "return 111"))
 	qt.Assert(t, qt.Not(qt.StringContains(head, "return 222")))
 }
+
+func TestRun_PathspecFileNulRequiresFromFile(t *testing.T) {
+	chdirTempRepo(t)
+
+	for _, command := range []string{"diff", "commit"} {
+		args := []string{command, "--pathspec-file-nul"}
+		if command == "commit" {
+			args = append(args, "-m", "fix: reject lone nul flag")
+		}
+		_, stderr, code := runApp(t, args...)
+
+		qt.Assert(t, qt.Equals(code, exitcode.InvalidUsage))
+		qt.Assert(t, qt.StringContains(stderr, "pathspec-file-nul"))
+	}
+}
+
+func TestRun_PathspecFromFileNulMatchesPositionals(t *testing.T) {
+	dir := chdirTempRepo(t)
+	writeAppFile(t, dir, "a.go", "package a\n\n// A returns one.\nfunc A() int {\n\treturn 111\n}\n\nfunc B() int {\n\treturn 222\n}\n")
+	path := filepath.Join(t.TempDir(), "targets")
+	if err := os.WriteFile(path, []byte("a.go:A\x00"), 0o644); err != nil {
+		t.Fatal(err)
+	}
+
+	stdout, _, code := runApp(t, "diff", "--porcelain", "--pathspec-from-file", path, "--pathspec-file-nul")
+	qt.Assert(t, qt.Equals(code, exitcode.Success))
+	qt.Assert(t, qt.StringContains(stdout, "a.go\tA\tMOD\t"))
+	qt.Assert(t, qt.Not(qt.StringContains(stdout, "\tB\t")))
+
+	_, _, code = runApp(t, "commit", "-m", "fix(a): bump A", "--pathspec-from-file", path, "--pathspec-file-nul")
+	qt.Assert(t, qt.Equals(code, exitcode.Success))
+	head := gitOut(t, dir, "cat-file", "-p", "HEAD:a.go")
+	qt.Assert(t, qt.StringContains(head, "return 111"))
+	qt.Assert(t, qt.Not(qt.StringContains(head, "return 222")))
+}
